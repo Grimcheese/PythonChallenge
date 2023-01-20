c9d5fca33e976a272e9bc5fb0b78e0a2c20e17ee
Grimcheese <albychawula@gmail.com>
2023-01-20 14:48:19 +0800

    Refactored each level into a method and can now choose to run as a script or import.

--- solution.py
+++ solution.py
@@ -1,23 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import requests
+
 home_url = "http://pythonchallenge.com"
 level_one = "http://pythonchallenge.com/pc/def/0.html"
 
 wait_prompt = "Enter when ready to move to next page..."
 
-print("Getting first level.")
 driver = webdriver.Firefox()
-driver.get(level_one)
 
-wait_input = input(wait_prompt)
+def go_level_one():
+    """Go to the first level and get solution."""
 
-print("Solving problem one...")
-result = 2**38
-level_two = f"{home_url}/pc/def/{result}.html"
-wait_input = input(wait_prompt)
+    print("Getting first level.")
 
-print("Getting level 2")
-driver.get(level_two)
+    driver.get(level_one)
 
-wait_input = input(wait_prompt)
+    print("Solving problem one...")
+    result = 2**38
+    level_two = f"{home_url}/pc/def/{result}.html"
+
+    wait_input = input(wait_prompt)
+    return level_two
+
+def go_level_two(url):
+    """Go to level 2 and work on solution."""
+    print("Getting level 2")
+    driver.get(url)
+
+    wait_input = input(wait_prompt)
+
+def run_main():
+    solution = go_level_one()
+    solution = go_level_two(solution)
+
+if __name__  == "__main__":
+    run_main()
