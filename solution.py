83b731409dddd91a8d4b54f33cd9808e1388d308
Grimcheese <albychawula@gmail.com>
2023-01-28 19:20:46 +0800

    Fixed level naming scheme to match the level shown on the website (start at index 0 not 1)

--- solution.py
+++ solution.py
@@ -13,21 +13,21 @@ wait_prompt = "Enter when ready to move to next page..."
 
 driver = webdriver.Firefox()
 
-def go_level_one():
-    """Go to the first level and get solution."""
+def go_level_zero():
+    """Go to the first level (0) and get solution."""
 
-    print("Getting first level.")
+    print("Getting level zero.")
 
     driver.get(level_one)
 
     print("Solving problem one...")
     result = 2**38
-    level_two = f"{home_url}/pc/def/{result}.html"
+    next_level = f"{home_url}/pc/def/{result}.html"
 
-    wait_input = input(wait_prompt)
-    return level_two
+    input(wait_prompt)
+    return next_level
 
-def level_two_decode(code):
+def level_one_decode(code):
 
     alpha = "abcdefghijklmnopqrstuvwxyz"
     translate = "cdefghijklmnopqrstuvwxyzab"
@@ -43,40 +43,40 @@ def level_two_decode(code):
 
     return decoded
 
-def go_level_two(url):
-    """Go to level 2 and work on solution."""
-    print("Getting level 2")
+def go_level_one(url):
+    """Go to level 1 and work on solution."""
+    print("Getting level 1")
     driver.get(url)
 
     r = requests.get(url)
     print(r.text)
 
-    print("Solving level 2")
+    print("Solving level 1")
     actual_url = f"{home_url}/pc/def/map.html"
 
 
     code = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj." 
-    decoded = level_two_decode(code)
+    decoded = level_one_decode(code)
     print(decoded)
 
-    new_url_ext = level_two_decode("map")
+    new_url_ext = level_one_decode("map")
     print(new_url_ext)
 
-    level_three_url = f"{challenge_url}{new_url_ext}.html"
+    next_url = f"{challenge_url}{new_url_ext}.html"
     
-    wait_input = input(wait_prompt)
+    input(wait_prompt)
 
-    return level_three_url
+    return next_url
 
-def go_level_three(url):
-    print("Getting level 3")
+def go_level_two(url):
+    print("Getting level 2")
     driver.get(url)
 
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     
     comment = soup.find_all(text=lambda text:isinstance(text, Comment))
-    
+
     # Count the number of times each character appears in a string
     in_string = comment[1]
 
@@ -86,16 +86,29 @@ def go_level_three(url):
         if character in char_count.keys():
             char_count[character] += 1
         else:
-            char_count[character] = 1 
+            char_count[character] = 1
     
     print("Characters found:")
     for key in char_count.keys():
         print(f"{key} : {char_count[key]}")
+    print("Solution: equality")
+    next_url =  f"{challenge_url}equality.html"
 
-    wait_input = input(wait_prompt)
+    input(wait_prompt)
+
+    return next_url
+
+def go_level_three(url):
+    """The third challenge to solve."""
+
+    print("Getting level 3")
+    driver.get(url)
+
+    input(wait_prompt)
 
 def run_main():
-    solution = go_level_one()
+    solution = go_level_zero()
+    solution = go_level_one(solution)
     solution = go_level_two(solution)
     solution = go_level_three(solution)
 
